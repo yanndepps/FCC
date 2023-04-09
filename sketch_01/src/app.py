bb55d023c1cb7a93a63fef16167a62c88a8995d0
yanndepps <yann.deperrot@protonmail.com>
2023-04-10 01:57:57 +0200

    add db

--- sketch_01/src/app.py
+++ sketch_01/src/app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SECRET_KEY"] = "thisisasecretkey"
+db = SQLAlchemy(app)
+
+
+# define a db model :
+# create a Note table, with automatic id, text as todo task
+# boolean holding the done value,
+# class Note(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     text = db.Column(db.Text)
+#     done = db.Column(db.Boolean)
+#     dateAdded = db.Column(db.DateTime, default=datetime.now())
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
