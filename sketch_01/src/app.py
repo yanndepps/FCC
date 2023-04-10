7f6688e70eb846bf2779fa99b8f832469cf18b2d
yanndepps <yann.deperrot@protonmail.com>
2023-04-10 02:15:43 +0200

    done db

--- sketch_01/src/app.py
+++ sketch_01/src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,18 +9,20 @@ app.config["SECRET_KEY"] = "thisisasecretkey"
 db = SQLAlchemy(app)
 
 
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+
 # define a db model :
 # create a Note table, with automatic id, text as todo task
 # boolean holding the done value,
-# class Note(db.Model):
-#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#     text = db.Column(db.Text)
-#     done = db.Column(db.Boolean)
-#     dateAdded = db.Column(db.DateTime, default=datetime.now())
-
-
-with app.app_context():
-    db.create_all()
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    text = db.Column(db.Text)
+    done = db.Column(db.Boolean)
+    dateAdded = db.Column(db.DateTime, default=datetime.now())
 
 
 @app.route("/")
@@ -38,4 +41,6 @@ def register():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
