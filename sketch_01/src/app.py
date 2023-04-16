3289b098b1c35256937d94ad568f3873812d1806
yanndepps <yann.deperrot@protonmail.com>
2023-04-16 17:05:53 +0200

    TODO: error msg

--- sketch_01/src/app.py
+++ sketch_01/src/app.py
@@ -95,6 +95,13 @@ def login():
     return render_template("login.html", form=form)
 
 
+@app.route("/logout", methods=["GET", "POST"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegisterForm()
