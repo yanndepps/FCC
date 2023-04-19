d3ede32b225073c2275d404da8b7b294e58d2345
yanndepps <yann.deperrot@protonmail.com>
2023-04-19 23:13:20 +0200

    wip display tasks fn

--- sketch_02/src/static/notes.js
+++ sketch_02/src/static/notes.js
@@ -1,13 +1,40 @@
 // init references
 const newTaskInput = document.querySelector("#new-task input");
-const taskDiv = document.querySelector("#tasks");
+const tasksDiv = document.querySelector("#tasks");
 let deleteTasks, editTasks, tasks;
 let updateNote;
 let count;
 
 // on window load
-window.onload() = () => {
+window.onload = () => {
 	updateNote = "";
 	count = Object.keys(localStorage).length;
 	displayTasks();
+};
+
+// display tasks
+const displayTasks = () => {
+	if (Object.keys(localStorage).length > 0) {
+		tasksDiv.style.display = "inline-block";
+	} else {
+		tasksDiv.style.display = "none";
+	}
+
+	// clear the tasks
+	tasksDiv.innerHTML = "";
+
+	// fetch all keys in LS
+	let tasks = Object.keys(localStorage);
+	tasks = tasks.sort();
+
+	for (let key of tasks) {
+		// get all values
+		let value = localStorage.getIem(key);
+		let taskInnerDiv = document.createElement("div");
+		taskInnerDiv.classList.add("task");
+		taskInnerDiv.setAttribute("id", key);
+		taskInnerDiv.innerHTML = `<span id="taskname">${key.split("_")[1]}</span>`;
+
+		// parse string <-> boolean
+	}
 };
