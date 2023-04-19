3116599f397933dd9974f73643773aef7c517be3
yanndepps <yann.deperrot@protonmail.com>
2023-04-19 22:33:02 +0200

    on window load

--- sketch_02/src/static/notes.js
+++ sketch_02/src/static/notes.js
@@ -2,5 +2,12 @@
 const newTaskInput = document.querySelector("#new-task input");
 const taskDiv = document.querySelector("#tasks");
 let deleteTasks, editTasks, tasks;
-let updateNote = "";
+let updateNote;
 let count;
+
+// on window load
+window.onload() = () => {
+	updateNote = "";
+	count = Object.keys(localStorage).length;
+	displayTasks();
+};
